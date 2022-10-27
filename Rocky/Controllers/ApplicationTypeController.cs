1ce22df1ce5c7b093c43099d65c744738ac8d66e
Dmytro Kotenko <dmytro.ko@qualer.com>
2022-10-27 21:21:35 +0300

    done HW1

---
+++ Rocky/Controllers/ApplicationTypeController.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System.Linq;
+
+namespace Rocky.Controllers
+{
+    public class ApplicationTypeController : Controller
+    {
+        private readonly AppDbContext _db;
+        public ApplicationTypeController(AppDbContext db)
+        {
+            _db = db;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _db.ApplicationType.ToList();
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(ApplicationType type)
+        {
+            _db.ApplicationType.Add(type);
+            _db.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+    }
+}
--- Rocky/Migrations/20221027172501_addApplicationTypeToDb.Designer.cs
+++ Rocky/Migrations/20221027172501_addApplicationTypeToDb.Designer.cs
@@ -2,15 +2,17 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Rocky.Data;
 
 namespace Rocky.Migrations
 {
     [DbContext(typeof(AppDbContext))]
-    partial class AppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20221027172501_addApplicationTypeToDb")]
+    partial class addApplicationTypeToDb
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
